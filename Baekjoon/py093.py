188838b49ab841d9b71eb3e6e75e048cb6bdd924
taeram <ryu.taeram@gmail.com>
2024-02-06 17:56:44 +0900

    String 02

--- Baekjoon/py093.py
+++ Baekjoon/py093.py
@@ -3,11 +3,6 @@ K, N =  map(int, input().split())
 LAN = []
 for _ in range(K):
     LAN.append(int(input()))
-LAN = sorted(LAN)
 
-min_lan = LAN[0] // 2
-cnt = 0
-for n in LAN:
-    cnt += n // min_lan
-
-print(cnt)
+for line in LAN:
+    
