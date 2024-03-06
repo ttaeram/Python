e5e16cd7ed81a94bdba8e4c0fd14071a1092ac56
ttaeram <ryu.taeram@gmail.com>
2024-03-06 18:01:10 +0900

    baekjoon

--- Baekjoon/py175.py
+++ Baekjoon/py175.py
@@ -1,8 +1,17 @@
-N = int(input())
+N, M, V = map(int, input().split())
+arr = [[] for _ in range(M)]
+for i in range(M):
+    a, b = map(int, input().split())
+    arr[a].append(b)
+    arr[b].append(a)
+print(arr)
+stack = [V]
+visited = [0] * (N + 1)
+while stack:
+    num = stack.pop()
+    if visited[num] == 0:
+        visited[num] = 1
+        print(num)
 
-if N % 5 == 0:
-    ans = -1
-else:
-    ans = 5 * (N - N // 5)
-
-print(ans)
+        for i in arr[num][::-1]:
+            stack.append(i)
