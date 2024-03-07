d5fa5c3826f5b9632401de82c132b3fbd6c817ee
ttaeram <ryu.taeram@gmail.com>
2024-03-07 17:59:08 +0900

    baekkjoon

--- Baekjoon/py175.py
+++ Baekjoon/py175.py
@@ -1,17 +1,27 @@
+from collections import deque
 N, M, V = map(int, input().split())
-arr = [[] for _ in range(M)]
+arr = [[] for _ in range(N + 1)]
 for i in range(M):
     a, b = map(int, input().split())
     arr[a].append(b)
     arr[b].append(a)
 print(arr)
+for l in arr:
+    l.sort()
+
 stack = [V]
+DFS = []
 visited = [0] * (N + 1)
 while stack:
     num = stack.pop()
     if visited[num] == 0:
         visited[num] = 1
-        print(num)
+        DFS.append(num)
 
         for i in arr[num][::-1]:
             stack.append(i)
+print(*DFS)
+
+stack_2 = [V]
+BFS = deque()
+visited_2 = [0] * (N + 1)
