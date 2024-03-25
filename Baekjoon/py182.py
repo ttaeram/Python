73949d1e82876590169c80415d96215025554e1b
Taeram <ryu.taeram@gmail.com>
2024-03-25 21:34:15 +0900

    bj

--- Baekjoon/py182.py
+++ Baekjoon/py182.py
@@ -1,10 +1,12 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 arr = list(map(int, input().split()))
-n_arr = sorted(arr)
-idx_arr = []
-for val in n_arr:
-    for i, res in enumerate(arr):
-        if res == val:
-            idx_arr.append(i)
-    
-print(idx_arr)
+n_arr = sorted(list(set(arr)))
+dic = {}
+for i in range(len(n_arr)):
+    dic[n_arr[i]] = i
+
+for j in arr:
+    print(dic[j], end = ' ')
