13da5f0103e02c4e7af74fb4bd2834707aa76558
Taeram <ryu.taeram@gmail.com>
2024-04-02 00:25:53 +0900

    bj

--- Baekjoon/py187.py
+++ Baekjoon/py187.py
@@ -7,17 +7,20 @@ arr = [[] for _ in range(N + 1)]
 for _ in range(M):
     A, B = map(int, input().split())
     arr[A].append(B)
+check = [0] * (N + 1)
 res = [int(1e9)] * (N + 1)
 stack = deque()
 stack.append((X, 0))
+check[X] = 1
 while stack:
     pos, cnt = stack.popleft()
-    if arr[pos]:
-        for i in range(len(arr[pos])):
-            if cnt + 1 > K:
-                continue
-            stack.append((arr[pos][i], cnt + 1))
-            res[arr[pos][i]] = min(res[arr[pos][i]], cnt + 1)
+    for i in arr[pos]:
+        if cnt + 1 > K:
+            continue
+        if check[i] == 0:
+            check[i] = 1
+            stack.append((i, cnt + 1))
+            res[i] = min(res[i], cnt + 1)
 
 flag = True
 for n in range(N + 1):
