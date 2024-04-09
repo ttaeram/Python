624301b052315a87f631088d9cd1b4af128bd849
Taeram <ryu.taeram@gmail.com>
2024-04-09 21:14:16 +0900

    bj

--- Baekjoon/py199.py
+++ Baekjoon/py199.py
@@ -3,22 +3,22 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-arr = [input().strip() for _ in range(N)]
+arr = [list(map(int, input().strip())) for _ in range(N)]
 
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
-stack = deque([(0, 0, 0)])
+stack = deque([(0, 0)])
 answer = []
-while stack:
-    x, y, d = stack.pop()
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] == 1:
-            stack.append((nx, ny, d + 1))
-            arr[nx][ny] = 0
-            if nx == N and ny == M:
-                answer.append(d)
-
-print(answer)
+def maze():
+    while stack:
+        x, y = stack.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] == 1:
+                stack.append((nx, ny))
+                arr[nx][ny] = arr[x][y] + 1
+    return arr[N-1][M-1]
+ans = maze()
+print(ans)
