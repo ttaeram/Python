3155e68f3a0ceb7e00f7b824264e4f3fb7220b80
Taeram <ryu.taeram@gmail.com>
2024-04-15 20:58:41 +0900

    bj

--- Baekjoon/py202.py
+++ Baekjoon/py202.py
@@ -7,18 +7,20 @@ S = input()
 s = 'I'
 
 def sol(N, M, S):
-    s = 'I'
-    for _ in range(N):
-        s += 'OI'
+    i = 0
     cnt = 0
-    for m in range(M):
-        if S[m] == 'O':
-            continue
-        elif m < M - 1 and S[m] == S[m + 1]:
-            continue
-        if S[m:m + (2 * N + 1)] == s:
+    ans = 0
+    while i < (M - 1):
+        if S[i : i + 3] == 'IOI':
+            i += 2
             cnt += 1
-    return cnt
+            if cnt == N:
+                ans += 1
+                cnt -= 1
+        else:
+            i += 1
+            cnt = 0
+    return ans
 
 ans = sol(N, M, S)
 print(ans)
--- Baekjoon/py204.py
+++ Baekjoon/py204.py
@@ -4,21 +4,46 @@ input = sys.stdin.readline
 T = int(input())
 for t in range(T):
     N = int(input())
-    conv = []
-    beer = 20
+    check = [[] for _ in range(N + 2)]
+    conv = [list(map(int, input().split())) for _ in range(N + 2)]
     for n in range(N + 2):
-        x, y = map(int, input().split())
-        if n == 0:
-            home = (x, y)
-        elif n == (N + 1):
-            rock = (x, y)
-        else:
-            conv.append((x, y))
-    if abs(rock[0] - home[0]) + abs(rock[1] - home[1]) <= 20 * 50:
-        ans = 'happy'
-        print(ans)
-        continue
-    conv.sort()
+        p0 = conv[n][0]
+        p1 = conv[n][1]
+        for i in range(N + 2):
+            h0 = conv[i][0]
+            h1 = conv[i][1]
+            if abs(h0 - p0) + abs(h1 - p1) <= 20 * 50:
+                check[n].append(i)
+
+    visited = [0] * (N + 2)
+    stack = [0]
+    flag = False
+    while stack:
+        pos = stack.pop()
+        if not visited[pos]:
+            visited[pos] = 1
+            for j in check[pos]:
+                stack.append(j)
+                if j == N + 1:
+                    ans = 'happy'
+                    flag = True
+                    break
+    if not flag:
+        ans = 'sad'
+    print(ans)
+            
+    #     x, y = map(int, input().split())
+    #     if n == 0:
+    #         home = (x, y)
+    #     elif n == (N + 1):
+    #         rock = (x, y)
+    #     else:
+    #         conv.append((x, y))
+    # if abs(rock[0] - home[0]) + abs(rock[1] - home[1]) <= 20 * 50:
+    #     ans = 'happy'
+    #     print(ans)
+    #     continue
+    # conv.sort()
 
     # if (abs(rock[0]) + abs(rock[1])) - (abs(home[0]) + abs(home[1])) <= 20 * 50:
     #     ans = 'happy'
@@ -32,33 +57,4 @@ for t in range(T):
     #         ans = 'sad'
     #     if (abs(rock[0]) + abs(rock[1])) - (abs(home[0]) + abs(home[1])) <= 20 * 50:
     #         ans = 'happy'
-    print(ans)
-
-T = int(input())
-
-for test_case in range(T):
-    N = int(input())
-    arr = []
-    arr.append(list(map(int, input().split())))
-    for _ in range(N):
-        arr.append(list(map(int, input().split())))
-    arr.append(list(map(int, input().split())))
-
-    possible = [[int(1e9)] * (N+2) for _ in range(N+2)]
-
-    for i in range(N+2):
-        for j in range(N+2):
-            if i != j:
-
-                diff = abs(arr[i][0] - arr[j][0]) + abs(arr[i][1] - arr[j][1])
-                if diff <= 1000:
-                    possible[i][j] = 1
-
-    for k in range(N+2):
-        for a in range(N+2):
-            for b in range(N+2):
-                possible[a][b] = min(possible[a][b], possible[a][k] + possible[k][b])
-    if possible[0][N+1] == int(1e9):
-        print("sad")
-    else:
-        print("happy")
+    # print(ans)
