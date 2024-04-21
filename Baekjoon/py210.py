adf2e95efc44dfb09db9a71fbed44084af8dae0c
Taeram <ryu.taeram@gmail.com>
2024-04-21 17:21:42 +0900

    bj

--- Baekjoon/py210.py
+++ Baekjoon/py210.py
@@ -8,51 +8,40 @@ odd = ('R', 'G')
 
 N = int(input())
 arr = [list(map(str, input().strip())) for _ in range(N)]
-check_1 = set()
-check_2 = set()
+check = set()
 cnt_1 = 0
 cnt_2 = 0
+
+def sol(r, c):
+    stack = deque([(r, c)])
+    check.add((r, c))
+    while stack:
+        x, y = stack.popleft()
+        color = arr[x][y]
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < N:
+                if arr[nx][ny] == color and (nx, ny) not in check:
+                    check.add((nx, ny))
+                    stack.append((nx, ny))
+
 for r in range(N):
     for c in range(N):
-        if (r, c) not in check_1:
-            stack_1 = deque([(r, c)])
-            while stack_1:
-                x, y = stack_1.popleft()
-                color = arr[x][y]
-                if (x, y) not in check_1:
-                    check_1.add((x, y))
-                    for i in range(4):
-                        nx = x + dx[i]
-                        ny = y + dy[i]
-                        if 0 <= nx < N and 0 <= ny < N:
-                            if arr[nx][ny] == color:
-                                stack_1.append((nx, ny))
+        if (r, c) not in check:
+            sol(r, c)
             cnt_1 += 1
-        if (r, c) not in check_2:
-            stack_2 = deque([(r, c)])
-            color_ = arr[r][c]
-            if color_ == 'B':
-                while stack_2:
-                    a, b = stack_2.popleft()
-                    if (a, b) not in check_2:
-                        check_2.add((a, b))
-                        for k in range(4):
-                            na = a + dx[k]
-                            nb = b + dy[k]
-                            if 0 <= na < N and 0 <= nb < N:
-                                if arr[nx][ny] == 'B':
-                                    stack_2.append((na, nb))
-                cnt_2 += 1
-            elif color_ != 'B':
-                while stack_2:
-                    a, b = stack_2.popleft()
-                    if (a, b) not in check_2:
-                        check_2.add((a, b))
-                        for j in range(4):
-                            na = a + dx[j]
-                            nb = b + dy[j]
-                            if 0 <= na < N and 0 <= nb < N:
-                                if arr[nx][ny] != 'B':
-                                    stack_2.append((na, nb))
-                cnt_2 += 1
+
+check = set()
+for r in range(N):
+    for c in range(N):
+        if arr[r][c] == 'R':
+            arr[r][c] = 'G'
+
+for r in range(N):
+    for c in range(N):
+        if (r, c) not in check:
+            sol(r, c)
+            cnt_2 += 1
+
 print(cnt_1, cnt_2)
--- Baekjoon/py214.py
+++ Baekjoon/py214.py
@@ -2,20 +2,31 @@ import sys
 input = sys.stdin.readline
 
 k = int(input())
-res = '0'
-
-def sol(res, k):
-    if len(res) >= k:
-        ans = res
-        return ans
-    res_s = ''
-    for i in res:
-        if i == '0':
-            res_s += '1'
-        else:
-            res_s += '0'
-    res += res_s
-    sol(res, k)
-
-ans = sol(res, k)
+def sol(k):
+    if k == 0:
+        return 0
+    elif k == 1:
+        return 1
+    elif k % 2 == 0:
+        sol(k//2)
+    else:
+        return 1 - sol(k//2)
+ans = sol(k - 1)
 print(ans)
+# res = '0'
+
+# def sol(res, k):
+#     if len(res) >= k:
+#         ans = res
+#         return ans
+#     res_s = ''
+#     for i in res:
+#         if i == '0':
+#             res_s += '1'
+#         else:
+#             res_s += '0'
+#     res += res_s
+#     sol(res, k)
+
+# ans = sol(res, k)
+# print(ans)
