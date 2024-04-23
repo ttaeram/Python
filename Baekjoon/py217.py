a745e7587016d9da2c974e44179b2cd067626325
ttaeram <ryu.taeram@gmail.com>
2024-04-23 18:00:39 +0900

    bj

--- Baekjoon/py217.py
+++ Baekjoon/py217.py
@@ -4,22 +4,55 @@ input = sys.stdin.readline
 N = int(input())
 M = int(input())
 if M != 0:
-    arr = list(map(int, input().split()))
+    arr = list(map(str, input().split()))
 
 button = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 pos = 100
 tar = str(N)
+T = len(tar)
 posstr = ''
-for n in len(N):
-    if N[n] in arr:
-        if N[n] < len(N) - 1:
-
-        i = 1
-        ch = int(N[n])
-        cand = []
-        while True:
-            if (ch - i) not in arr:
-                if (ch + i) in arr:
-                    posstr += str(ch - i)
+for n in range(T-1, -1, -1):
+    if tar[n] in arr:
+        if n == T - 1:
+            ch = int(tar[n])
+            i = 1
+            while True:
+                if str(ch + i) not in arr:
+                    if (ch + i) < 10:
+                        posstr = str(ch + i)
+                        break
                 else:
-                    posstr += str(ch + i)
+                    if str(ch - i) not in arr:
+                        if (ch - i) > -1:
+                            posstr = str(ch - i)
+                            break
+                i += 1
+        else:
+            cha = int(posstr)
+            ch = int(tar[n])
+            i = 1
+            while True:
+                if str(ch + i) not in arr:
+                    if (ch + i) < 10:
+                        posstr = str(ch + i) + posstr
+                        break
+                else:
+                    if str(ch - i) not in arr:
+                        if (ch - i) > -1:
+                            posstr = str(ch - i) + posstr
+                            break
+                i += 1
+    else:
+        posstr = tar[n] + posstr
+print(posstr)
+        # if n < len(N) - 1:
+        #     pass
+        # i = 1
+        # ch = int(N[n])
+        # cand = []
+        # while True:
+        #     if (ch - i) not in arr:
+        #         if (ch + i) in arr:
+        #             posstr += str(ch - i)
+        #         else:
+        #             posstr += str(ch + i)
---
+++ Baekjoon/py218.py
@@ -0,0 +1,72 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+command = ['D', 'S', 'L', 'R']
+for t in range(T):
+    A, B = map(int, input().split())
+    stack = deque([(A, '')])
+    check = set()
+    flag = True
+    while True:
+        A, com = stack.popleft()
+        if A not in check:
+            for c in command:
+                AA = A
+                comm = com
+                if c == 'D':
+                    AA = 2 * AA
+                    if AA > 9999:
+                        AA = AA % 10000
+                    if AA == B:
+                        ans = comm + 'D'
+                        flag = False
+                        break
+                    stack.append((AA, comm + 'D'))
+                    check.add(AA)
+                elif c == 'S':
+                    if AA == 0:
+                        AA = 9999
+                    else:
+                        AA = AA - 1
+                    if AA == B:
+                        ans = comm + 'S'
+                        flag = False
+                        break
+                    stack.append((AA, comm + 'S'))
+                    check.add(AA)
+                elif c == 'L':
+                    sA = str(AA)
+                    if len(sA) % 4 != 0:
+                        nA = ''
+                        for i in range(4 - (len(sA) % 4)):
+                            nA += '0'
+                        sA = nA + sA
+                    sA = sA[1:4] + sA[0]
+                    AA = int(sA)
+                    if AA == B:
+                        ans = comm + 'L'
+                        flag = False
+                        break
+                    stack.append((AA, comm + 'L'))
+                    check.add(AA)
+                elif c == 'R':
+                    sA = str(AA)
+                    if len(sA) % 4 != 0:
+                        nA = ''
+                        for i in range(4 - (len(sA) % 4)):
+                            nA += '0'
+                        sA = nA + sA
+                    sA = sA[3] + sA[0:3]
+                    AA = int(sA)
+                    if AA == B:
+                        ans = comm + 'R'
+                        flag = False
+                        break
+                    stack.append((AA, comm + 'R'))
+                    check.add(AA)
+        if not flag:
+            break
+        print(stack)
+    print(ans)
