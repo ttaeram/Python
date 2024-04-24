72932c9edc6551adec6cd1c3ff48b4c71aa5dcac
Taeram <ryu.taeram@gmail.com>
2024-04-24 20:48:36 +0900

    bj

--- Baekjoon/py218.py
+++ Baekjoon/py218.py
@@ -11,62 +11,64 @@ for t in range(T):
     flag = True
     while True:
         A, com = stack.popleft()
-        if A not in check:
-            for c in command:
-                AA = A
-                comm = com
-                if c == 'D':
-                    AA = 2 * AA
-                    if AA > 9999:
-                        AA = AA % 10000
-                    if AA == B:
-                        ans = comm + 'D'
-                        flag = False
-                        break
+        for c in command:
+            AA = A
+            comm = com
+            if c == 'D':
+                AA = 2 * AA
+                if AA > 9999:
+                    AA = AA % 10000
+                if AA == B:
+                    ans = comm + 'D'
+                    flag = False
+                    break
+                if AA not in check:
                     stack.append((AA, comm + 'D'))
                     check.add(AA)
-                elif c == 'S':
-                    if AA == 0:
-                        AA = 9999
-                    else:
-                        AA = AA - 1
-                    if AA == B:
-                        ans = comm + 'S'
-                        flag = False
-                        break
+            elif c == 'S':
+                if AA == 0:
+                    AA = 9999
+                else:
+                    AA = AA - 1
+                if AA == B:
+                    ans = comm + 'S'
+                    flag = False
+                    break
+                if AA not in check:
                     stack.append((AA, comm + 'S'))
                     check.add(AA)
-                elif c == 'L':
-                    sA = str(AA)
-                    if len(sA) % 4 != 0:
-                        nA = ''
-                        for i in range(4 - (len(sA) % 4)):
-                            nA += '0'
-                        sA = nA + sA
-                    sA = sA[1:4] + sA[0]
-                    AA = int(sA)
-                    if AA == B:
-                        ans = comm + 'L'
-                        flag = False
-                        break
+            elif c == 'L':
+                sA = str(AA)
+                if len(sA) % 4 != 0:
+                    nA = ''
+                    for i in range(4 - (len(sA) % 4)):
+                        nA += '0'
+                    sA = nA + sA
+                sA = sA[1:4] + sA[0]
+                AA = int(sA)
+                if AA == B:
+                    ans = comm + 'L'
+                    flag = False
+                    break
+                if AA not in check:
                     stack.append((AA, comm + 'L'))
                     check.add(AA)
-                elif c == 'R':
-                    sA = str(AA)
-                    if len(sA) % 4 != 0:
-                        nA = ''
-                        for i in range(4 - (len(sA) % 4)):
-                            nA += '0'
-                        sA = nA + sA
-                    sA = sA[3] + sA[0:3]
-                    AA = int(sA)
-                    if AA == B:
-                        ans = comm + 'R'
-                        flag = False
-                        break
+            elif c == 'R':
+                sA = str(AA)
+                if len(sA) % 4 != 0:
+                    nA = ''
+                    for i in range(4 - (len(sA) % 4)):
+                        nA += '0'
+                    sA = nA + sA
+                sA = sA[3] + sA[0:3]
+                AA = int(sA)
+                if AA == B:
+                    ans = comm + 'R'
+                    flag = False
+                    break
+                if AA not in check:
                     stack.append((AA, comm + 'R'))
                     check.add(AA)
         if not flag:
             break
-        print(stack)
     print(ans)
--- Baekjoon/py220.py
+++ Baekjoon/py220.py
@@ -1,16 +1,38 @@
 import sys
 input = sys.stdin.readline
 
-def sol():
-    le = []
-    sn = []
-    N, M = map(int, input().split())
-    for _ in range(N):
-        a, b = map(int, input().split())
-        le.append((a, b))
-    for _ in range(M):
-        a, b = map(int, input().split())
-        sn.append((a, b))
-    arr = [0] * 101
-    for i in range(101):
-        
+T = int(input())
+for t in range(T):
+    M, N, x, y = map(int, input().split())
+    flag = True
+    while x <= M * N:
+        if (x - y) % N == 0:
+            ans = x
+            flag = False
+            break
+        x += M
+    if flag:
+        ans = -1
+    print(ans)
+    # a, b = 1, 1
+    # cnt = 1
+    # flag = True
+    # while True:
+    #     if a == M:
+    #         a = 1
+    #     else:
+    #         a += 1
+    #     if b == N:
+    #         b = 1
+    #     else:
+    #         b += 1
+    #     cnt += 1
+    #     if a == x and b == y:
+    #         break
+    #     elif a == 1 and b == 1:
+    #         flag = False
+    #         break
+    # if flag:
+    #     print(cnt)
+    # else:
+    #     print(-1)
