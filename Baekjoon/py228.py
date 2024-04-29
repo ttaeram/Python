2b3e04fe68a51ae5660c5d38a9ed45f84bb5290c
Taeram <ryu.taeram@gmail.com>
2024-04-29 20:57:36 +0900

    bj

--- Baekjoon/py228.py
+++ Baekjoon/py228.py
@@ -2,7 +2,15 @@ import sys
 input = sys.stdin.readline
 
 def sol(n):
-    if n == M:
-    
+    if len(arr) == M:
+        print(*arr)
+        return
+    for i in range(n, N + 1):
+        if i not in arr:
+            arr.append(i)
+            sol(i + 1)
+            arr.pop()
 
 N, M = map(int, input().split())
+arr = []
+sol(1)
