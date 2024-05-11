6530001afaf14a6d85d417dee647bc621276a522
Taeram <ryu.taeram@gmail.com>
2024-05-11 23:35:45 +0900

    bj

---
+++ Baekjoon/py243.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+def sol(n):
+    if len(arr) == M:
+        print(*arr)
+    else:
+        for i in range(1, N + 1):
+            arr.append(i)
+            sol(i + 1)
+            arr.pop()
+
+N, M = map(int, input().split())
+arr = []
+sol(1)
