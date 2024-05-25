ea03def1cb85a2855e06867dc4fc49c2542e9982
Taeram <ryu.taeram@gmail.com>
2024-05-25 17:27:42 +0900

    bj

--- Baekjoon/py252.py
+++ Baekjoon/py252.py
@@ -13,26 +13,52 @@ for r in range(N):
     if not shark:
         for c in range(N):
             if a[c] == 9:
-                shark = (r, c, 2, 0, 0)
+                shark = (r, c)
                 break
     arr.append(a)
-check = set()
-stack = deque([shark])
-while stack:
-    x, y, s, c, t = stack.pop()
-    if (x, y) not in check:
-        print((x, y, s, c, t))
-        check.add((x, y))
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < N and arr[nx][ny] <= s:
-                if arr[nx][ny] == 0:
-                    stack.append((nx, ny, s, c, t + 1))
-                else:
-                    arr[nx][ny] = 0
-                    if s == c + 1:
-                        stack.append((nx, ny, s + 1, 0, t + 1))
-                    else:
-                        stack.append((nx, ny, s, c + 1, t + 1))
-print(t-1)
+ans = 0
+def bfs(x, y):
+    visited = [[0] * N for _ in range(N)]
+    queue = deque([[x,y]])
+    cand = []
+
+    visited[x][y] = 1
+
+    while queue:
+        i, j = queue.popleft()
+
+        for idx in range(4):
+            ii, jj = i + dx[idx] , j + dy[idx]
+
+            if 0 <= ii and ii < N and 0 <= jj and jj < N and visited[ii][jj] == 0:
+                if arr[x][y] > arr[ii][jj] and arr[ii][jj] != 0:
+                    visited[ii][jj] =  visited[i][j] + 1
+                    cand.append((visited[ii][jj] - 1, ii, jj))
+                elif arr[x][y] == arr[ii][jj]:
+                    visited[ii][jj] =  visited[i][j] + 1
+                    queue.append([ii,jj])
+                elif arr[ii][jj] == 0:
+                    visited[ii][jj] =  visited[i][j] + 1
+                    queue.append([ii,jj])
+    return sorted(cand, key = lambda x: (x[0], x[1], x[2]))
+
+
+i, j = shark
+size = [2, 0]
+while True:
+    arr[i][j] = size[0]
+    cand = deque(bfs(i,j))
+    if not cand:
+        break 
+    step, xx, yy = cand.popleft()
+    ans += step
+    size[1] += 1
+    
+    if size[0] == size[1]:
+        size[0] += 1
+        size[1] = 0
+
+    arr[i][j] = 0
+    i, j = xx, yy
+        
+print(ans)
