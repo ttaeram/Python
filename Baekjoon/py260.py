f0670ca59084594718bf9b301876669a3702fd76
Taeram <ryu.taeram@gmail.com>
2024-06-28 16:58:32 +0900

    bj

--- Baekjoon/py260.py
+++ Baekjoon/py260.py
@@ -6,20 +6,67 @@ dice_noon = {1: [0, 4, 3, 2, 5, 6], 2: [0, 4, 3, 6, 1, 5], 3: [0, 6, 1, 5, 2, 4]
 direction = {1: (0, 1), 2: (0, -1), 3: (-1, 0), 4: (1, 0)}
 arr = [list(map(int, input().split())) for _ in range(N)]
 command = list(map(int, input().split()))
-dice = 1
+dice = {'top': 0, 'bottom': 0, 'left': 0, 'right': 0, 'front': 0, 'back': 0}
+
+def roll_east(dice):
+    return {
+        'top': dice['left'],
+        'bottom': dice['right'],
+        'left': dice['bottom'],
+        'right': dice['top'],
+        'front': dice['front'],
+        'back': dice['back']
+    }
+
+def roll_west(dice):
+    return {
+        'top': dice['right'],
+        'bottom': dice['left'],
+        'left': dice['top'],
+        'right': dice['bottom'],
+        'front': dice['front'],
+        'back': dice['back']
+    }
+
+def roll_north(dice):
+    return {
+        'top': dice['front'],
+        'bottom': dice['back'],
+        'left': dice['left'],
+        'right': dice['right'],
+        'front': dice['bottom'],
+        'back': dice['top']
+    }
+
+def roll_south(dice):
+    return {
+        'top': dice['back'],
+        'bottom': dice['front'],
+        'left': dice['left'],
+        'right': dice['right'],
+        'front': dice['top'],
+        'back': dice['bottom']
+    }
 for c in command:
     dx, dy = direction[c]
     nx = x + dx
     ny = y + dy
     if 0 <= nx < N and 0 <= ny < M:
         x, y = nx, ny
-    else:
-        continue
 
-    dice = dice_noon[dice][c]
-    if arr[x][y] == 0:
-        arr[x][y] = dice_noon[dice_noon[dice][5]][0]
-    else:
-        dice_noon[dice_noon[dice][5]][0] = arr[x][y]
-        arr[x][y] = 0
-    print(dice_noon[dice][0])
+        if c == 1:
+            dice = roll_east(dice)
+        elif c == 2:
+            dice = roll_west(dice)
+        elif c == 3:
+            dice = roll_north(dice)
+        elif c == 4:
+            dice = roll_south(dice)
+
+        if arr[x][y] == 0:
+            arr[x][y] = dice['bottom']
+        else:
+            dice['bottom'] = arr[x][y]
+            arr[x][y] = 0
+            
+        print(dice['top'])
