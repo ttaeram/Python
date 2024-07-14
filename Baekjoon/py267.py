3d00713a075cff922a7236b092269388631ca159
Taeram <ryu.taeram@gmail.com>
2024-07-14 16:19:36 +0900

    bj

--- Baekjoon/py267.py
+++ Baekjoon/py267.py
@@ -1,15 +1,51 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
 N = int(input())
-arr = []
-for _ in range(N):
-    semi = list(map(int, input().split()))
-    semi = deque(semi)
-    arr.append(semi)
-print(arr)
+arr = [list(map(int, input().split())) for _ in range(N)]
+check = [0] * 9
+pos = [0] * 9
+pos[3] = 0
+check[3] = 1
+ans = 0
 
-for n in range(N):
-    one = arr[n][0]
-    feild = [0] * 4
+def game(res):
+    global ans
+    if res == 9:
+        idx = 0
+        score = 0
+        for g in arr:
+            out = 0
+            lu1, lu2, lu3 = 0, 0, 0
+            while out <= 2:
+                taja = pos[idx]
+                if g[taja] == 0:
+                    out += 1
+                elif g[taja] == 1:
+                    score += lu3
+                    lu1, lu2, lu3 = 1, lu1, lu2
+                elif g[taja] == 2:
+                    score += lu2 + lu3
+                    lu1, lu2, lu3 = 0, 1, lu1
+                elif g[taja] == 3:
+                    score += lu1 + lu2 + lu3
+                    lu1, lu2, lu3 = 0, 0, 1
+                else:
+                    score += lu1 + lu2 + lu3 + 1
+                    lu1, lu2, lu3 = 0, 0, 0
+                idx += 1
+                idx %= 9
+        ans = max(ans, score)
+        return
+    
+    for i in range(9):
+        if check[i]:
+            continue
+        else:
+            check[i] = 1
+            pos[i] = res
+            game(res + 1)
+            check[i] = 0
+            pos[i] = 0
+game(1)
+print(ans)
