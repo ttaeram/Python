9aeef02d2a48556c87a62ebed48a60b0f8bbe764
Taeram <ryu.taeram@gmail.com>
2025-01-27 22:28:45 +0900

    update: bj 14938

--- Baekjoon/py317.py
+++ Baekjoon/py317.py
@@ -1,3 +1,9 @@
 import sys
 input = sys.stdin.readline
 
+N, M, R = map(int, input().split())
+items = list(map(int, input().split()))
+arr = []
+for _ in range(R):
+    a, b, l = map(int, input().split())
+    arr.append((a, b, l))
