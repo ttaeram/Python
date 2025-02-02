c6520a351c13061290a5cd45fb72168d029b496d
Taeram <ryu.taeram@gmail.com>
2025-02-02 17:33:29 +0900

    update: bj 5639

--- Baekjoon/py318.py
+++ Baekjoon/py318.py
@@ -1,2 +1,29 @@
 import sys
+sys.setrecursionlimit(10 ** 9)
 input = sys.stdin.readline
+
+arr = []
+while True:
+    try:
+        n = int(input())
+        arr.append(n)
+
+    except:
+        break
+
+def sol(start, end):
+    if start > end:
+        return
+    
+    mid = end + 1
+
+    for i in range(start + 1, end + 1):
+        if arr[i] > arr[start]:
+            mid = i
+            break
+    
+    sol(start + 1, mid - 1)
+    sol(mid, end)
+    print(arr[start])
+
+sol(0, len(arr) - 1)
