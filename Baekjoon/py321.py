7a36693b361bd83514320b80c48b24114ca94f49
Taeram <ryu.taeram@gmail.com>
2025-02-07 17:00:33 +0900

    update: bj 2448

--- Baekjoon/py321.py
+++ Baekjoon/py321.py
@@ -2,5 +2,24 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
+arr = [[" "] * 2 * N for _ in range(N)]
 
-print(N)
+def sol(i, j, phase):
+    if phase == 3:
+        arr[i][j] = "*"
+        arr[i + 1][j - 1] = "*"
+        arr[i + 1][j + 1] = "*"
+
+        for k in range(-2, 3):
+            arr[i + 2][j - k] = "*"
+        
+    else:
+        newPhase = phase // 2
+        sol(i, j, newPhase)
+        sol(i + newPhase, j - newPhase, newPhase)
+        sol(i + newPhase, j + newPhase, newPhase)
+
+sol(0, N - 1, N)
+
+for ans in arr:
+    print("".join(ans))
